abc11aceb05e66941ae4df9044b49b046a8cf49e
Misha Lazarev <125580810+MsLazarev@users.noreply.github.com>
2023-11-14 19:36:00 +0500

    full code with sklearn

--- main.py
+++ main.py
@@ -29,6 +29,21 @@ def change_sex(row):
 df['Embarked'].fillna('S', inplace=True)
 df['Age'] = df.apply(change_age, axis=1)
 df['Sex'] = df.apply(change_sex, axis=1)
-df[list(pd.get_dummies(df["Embarked"]).columns)] = pd.get_dummies(df["Embarked"], dtype=int)
+df[list(pd.get_dummies(df["Embarked"]).columns)] = pd.get_dummies(df["Embarked"])
 df.drop(['PassengerId', 'Name', 'Ticket', 'Cabin', 'Embarked'], axis=1, inplace=True)
-print(df.head())
+
+print(df.info())
+x = df.drop('Survived', axis = 1)
+y = df['Survived']
+
+x_train, y_train, x_test, y_test = train_test_split(x, y, test_size=0.25)
+
+sc = StandardScaler()
+x_train = sc.fit_transform(x_train)
+x_test = sc.transform(x_test)
+classifier = KNeighborsClassifier(n_neighbors=3)
+classifier.fit(x_train, y_train)
+
+y_pred = classifier.predict(x_test)
+
+print(accuracy_score(y_test, y_pred) * 100)
